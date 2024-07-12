8642beb90354aa569ad52b5566c94b142de218b5
asfari7 <muhammadasnafi4@gmail.com>
2024-07-12 15:17:06 +0700

    add signIn Endpoint

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
 const indexRoutes = require("./routes/indexRoutes");
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 3600000 * 24, secure: false, sameSite: "strict" },
+  })
+);
 app.use(indexRoutes);
 
 app.get("/", (req, res) => {
