0796c536f5c904d2bbe1a7385936fef7cd594d5f
asfari7 <muhammadasnafi4@gmail.com>
2024-07-16 13:01:07 +0700

    fix prisma config

--- prisma/edge.js
+++ prisma/edge.js
@@ -151,6 +151,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/index.js
+++ prisma/index.js
@@ -152,6 +152,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
