fc49b12c814920660d3c06c574384809559ff001
asfari7 <muhammadasnafi4@gmail.com>
2024-07-16 13:19:12 +0700

    fix bug

--- index.js
+++ index.js
@@ -22,10 +22,6 @@ app.use(
 );
 app.use(indexRoutes);
 
-app.get("/", (req, res) => {
-  res.send("server is running");
-});
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
